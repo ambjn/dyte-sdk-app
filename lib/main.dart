0031042b1404cdb49cd563fef411afecfcb38deb
Amber Jain <jainanuamber@gmail.com>
2022-08-04 15:05:48 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Dyte SDK',
       theme: ThemeData.dark(),
       home: HomePage(),
